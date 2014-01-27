d85c6369cceca9ff0ae75bcf0fa99769a1816480
Michael Lyons <no@email.please>
2014-01-28 07:58:23 +1100

    Added example site

---
+++ src/NSec.ExampleSite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using NSec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSec.ExampleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UnsuccessfulLoginAttempt()
+        {
+            NSecManager.ReportSecurityEvent(EventType.UnsuccessfulLoginAttempt);
+            return View("Index");
+        }
+    }
+}
---
+++ src/NSec.ExampleSite/NSecThresholds/UnsuccessfulLoginAttempt.cs
@@ -0,0 +1,23 @@
+﻿using NSec.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSec.ExampleSite.NSecThresholds
+{
+    public class UnsuccessfulLoginAttempt : Threshold
+    {
+        public UnsuccessfulLoginAttempt()
+        {
+            this.Conditions = new ThresholdConditions()
+            {
+                EventType = EventType.UnsuccessfulLoginAttempt,
+                MinimumSecurityEvents = 5,
+                Period = TimeSpan.Zero
+            };
+
+            this.Reactions.Add(new ThresholdReaction(Config.Action.Throttle, AttackerComparison.AnonymousId));
+        }
+    }
+}
